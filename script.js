4e77eaf0b94851eb5fa32d9c81e1c21bc4f45fb9
Abhishek Mishra <iabm3568@gmail.com>
2025-11-15 12:43:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+    // Store width and height as private properties
+    this._width = width;
+    this._height = height;
+	}
 
-class Square extends Animal {}
+	// Getter for width
+  get width() {
+    return this._width;
+  }
+  
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+  
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Animal {
+	constructor(side) {
+    // Call parent Rectangle constructor with side for both width and height
+    super(side, side);
+    
+    // Store the side value for easy reference
+    this._side = side;
+  }
+  
+  // Method to calculate perimeter
+  getPerimeter() {
+    return this._side * 4;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
